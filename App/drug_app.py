7259e0aa607fdc260f3a70d04888ed4aedb0791f
NaBai-Smart-it <s25099@pjwstk.edu.pl>
2025-06-13 19:27:13 +0200

    update trusted types

--- App/drug_app.py
+++ App/drug_app.py
@@ -1,7 +1,8 @@
 import gradio as gr
 from skops.io import load, get_untrusted_types
 
-pipe = load(file="Model/drug_pipeline.skops", trusted=get_untrusted_types())
+trusted_types = get_untrusted_types()
+pipe = load(file="Model/drug_pipeline.skops", trusted=trusted_types)
 
 def predict_drug(age, sex, bp, chol, na_k):
     return f"Predicted Drug: {pipe.predict([[age, sex, bp, chol, na_k]])[0]}"
